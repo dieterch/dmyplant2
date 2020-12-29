49a102bf6eb93b447ad6272e2d6066e82e038e7e
dieterchvatal <dieter.chvatal@gmail.com>
2020-12-29 17:00:05 +0100

    renamed to dmyplant2

--- dmyplant/__init__.py
+++
@@ -1,8 +0,0 @@
-﻿__version__ = "0.0.1"
-
-from dmyplant.support import cred
-from dmyplant.dMyplant import MyPlant
-from dmyplant.dValidation import Validation
-from dmyplant.dEngine import Engine, EngineReadOnly
-import dmyplant.dReliability
-from dmyplant.dPlot import demonstrated_Reliabillity_Plot
