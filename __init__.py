cf2c008de3d1301d656c77d8826fe5384359857e
dieterchvatal <dieter.chvatal@gmail.com>
2021-01-18 10:41:47 +0100

    files hin und her kopiert

---
+++ __init__.py
@@ -0,0 +1,8 @@
+﻿__version__ = "0.0.1"
+
+from dmyplant2.support import cred
+from dmyplant2.dMyplant import MyPlant
+from dmyplant2.dValidation import Validation
+from dmyplant2.dEngine import Engine, EngineReadOnly
+import dmyplant2.dReliability
+from dmyplant2.dPlot import demonstrated_Reliabillity_Plot
